39202c753a865f73011a12ac5ba82a5520c81e88
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2024-09-24 14:49:17 -0300

    Added Flock scripts
    The behavior system for the flock scripts have been added to the project, needs some work but it's getting there
    
    The birds prefab might need to be changed based off testing
    
    This project is to show how a bird behavior using AI to interact with each other

---
+++ Assets/Scripts/FlockAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class FlockAgent : MonoBehaviour
+{
+    Collider2D agentCollider; // The collider of the agent, what we will use to detect other agents, it's not public because we don't want to expose it to the inspector
+    public Collider2D AgentCollider { get { return agentCollider; } } // Property to get the collider of the agent, we use this to get the collider from the FlockBehavior script
+    
+    void Start()
+    {
+        agentCollider = GetComponent<Collider2D>();
+    }
+
+    public void Move(Vector2 velocity) // Move the agent, the Flocks
+    {
+        transform.up = velocity; // Rotate the agent to face the direction it's moving
+        transform.position += (Vector3)velocity * Time.deltaTime; // Framerate independent movement
+        
+    }
+}
